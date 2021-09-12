874f6bdef9a0d8b838705df51f09f48d81dfdc18
seon <seonsook.lee@twolinecode.com>
2021-09-12 22:01:42 +0900

    login, signUp 동작코드 옮김.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,48 @@
 import React from "react";
 
 const Login = () => {
+    const ex401 = '자격 증명에 실패하였습니다.';
+    const url = 'https://jssampletest.herokuapp.com/api/auth/login';
+
+    function fetchPost(data) {
+        return fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+    }
+
+    function login(e) {
+        e.preventDefault();
+        let email = document.getElementById('email').value;
+        let password = document.getElementById('password').value;
+
+        let data = { email: email, password: password };
+        // console.log(data);
+
+        fetchPost(data)
+            .then(async (response) => {
+                const result = await response.json();
+                console.log("data", result.data);
+                if (result.data === ex401) return;
+                if (result.data.accessToken) localStorage.setItem('accessToken', result.data.accessToken);
+                // location.href = 'memberControl.html';
+            })
+            .catch((e) => {});
+
+    }
+
     return(
         <div>
             <h1>Login</h1>
-            <form Id="loginForm">
+            <form id="loginForm">
                 <label htmlFor="email">email</label>
                 <input type="text" id="email"/>
                     <label htmlFor="password">password</label>
                     <input type="password" id="password"/>
-                    <button type="submit">login</button>
+                    <button type="submit" onClick={login}>login</button>
             </form>
         </div>
     )
--- src/shared/App.js
+++ src/shared/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <Route path="/" component={ Home }></Route>
+                <Route exact path="/" component={ Home }></Route>
                 <Route path="/login" component={ Login }></Route>
                 <Route path="/sign-up" component={ SignUp }></Route>
                 <Route path="/Member-control" component={ MemberControl }></Route>
