b839d4b85d157adb3d9496668e83ea6661447c77
이선숙 <seonsook.lee@twolinecode.com>
2021-09-19 19:18:42 +0900

    axios 설치 및 로그인 코드 적용.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,51 +1,37 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
+import url from '../utils/url';
 
 const Login = () => {
     const ex401 = '자격 증명에 실패하였습니다.';
-    const url = 'https://jssampletest.herokuapp.com/api/auth/login';
 
-    function fetchPost(data) {
-        return fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
-    }
-
-    function login(e) {
+    const login = (e) => {
         e.preventDefault();
         let email = document.getElementById('email').value;
         let password = document.getElementById('password').value;
+        let data = {email: email, password: password};
 
-        let data = { email: email, password: password };
-        // console.log(data);
-
-        fetchPost(data)
-            .then(async (response) => {
-                const result = await response.json();
-                console.log("data", result.data);
-                if (result.data === ex401) return;
-                if (result.data.accessToken) localStorage.setItem('accessToken', result.data.accessToken);
-                // location.href = 'memberControl.html';
+        axios.post(url+'login', data)
+            .then(response => {
+                const result = response.data;
+                if(result.data === ex401) return;
+                if(result.data.accessToken) localStorage.setItem('accessToken', result.data.accessToken);
             })
-            .catch((e) => {});
-
+            .catch(response => { console.log(response) });
     }
 
-    return(
-        <div>
-            <h1>Login</h1>
-            <form id="loginForm">
-                <label htmlFor="email">email</label>
-                <input type="text" id="email"/>
+    return (
+            <div>
+                <h1>Login</h1>
+                <form id="loginForm">
+                    <label htmlFor="email">email</label>
+                    <input type="text" id="email"/>
                     <label htmlFor="password">password</label>
                     <input type="password" id="password"/>
                     <button type="submit" onClick={login}>login</button>
-            </form>
-        </div>
-    )
-}
+                </form>
+            </div>
+        )
+    }
 
 export default Login;
